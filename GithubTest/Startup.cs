8c540e37a9bd754cd559ddb7f5d8f00cba80156b
Alec Lankford <adlankford@crimson.ua.edu>
2018-11-14 14:33:03 -0600

    Add project files.

---
+++ GithubTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GithubTest.Startup))]
+namespace GithubTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
